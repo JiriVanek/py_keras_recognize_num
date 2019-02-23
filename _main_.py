ca6492350fd14080408ed809b547a66d9babc18b
vanek2 <vanek2@rti.zcu.cz>
2019-02-23 14:31:54 +0100

    added time measurement

--- _main_.py
+++ _main_.py
@@ -1,6 +1,7 @@
 import numpy
 from keras import backend as K
 import keras
+import time
 from keras.datasets import mnist
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -11,6 +12,8 @@ from keras.models import Sequential
 from keras.utils import np_utils
 
 K.set_image_dim_ordering('th')
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
 if 'tensorflow' == K.backend():
     import tensorflow as tf
@@ -39,31 +42,41 @@ def keras_model():
     network.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return network
 
+allruns=0;
+allscore=0;
+for x in range(1, 51):
+    # fix random seed for reproducibility
+    seed = 7
+    numpy.random.seed(seed)
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+    # load data
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    # reshape to be [samples][pixels][width][height]
+    X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+    # normalize inputs from 0-255 to 0-1
+    X_train = X_train / 255
+    X_test = X_test / 255
+    # one hot encode outputs
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
+    num_classes = y_test.shape[1]
 
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
-tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-# reshape to be [samples][pixels][width][height]
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
-
-# build the model
-model = keras_model()
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=15, batch_size=200,callbacks=[tbCallBack])
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+    start = time.time()
+    # build the model
+    model = keras_model()
+    # Fit the model
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=15, batch_size=200,callbacks=[tbCallBack])
+    # Final evaluation of the model
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    end = time.time()
+    print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+    allscore+=scores[1]
+    run=end-start
+    allruns +=run
+    print("Avg Time "+ str(allruns/x) + "run " + str(x))
+    print("Avg Error: %.2f%%" % (100 - (allscore/x) * 100))
+    resultTime=allruns/x
 
 
 
